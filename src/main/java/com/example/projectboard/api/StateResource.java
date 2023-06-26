c89ed455f12526c57a24a29f997c0a5f1c4d1132
ziogenorwekh <ziogenorwekh@gmail.com>
2023-06-26 19:09:26 +0900

    test

--- src/main/java/com/example/projectboard/api/StateResource.java
+++ src/main/java/com/example/projectboard/api/StateResource.java
@@ -8,9 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@CrossOrigin(origins = "http://172.17.0.1:3000", methods = RequestMethod.GET, allowedHeaders = "application/json")
+//@CrossOrigin(origins = "http://172.17.0.1:3000", methods = RequestMethod.GET, allowedHeaders = "application/json")
 public class StateResource {
-
 
     @RequestMapping(method = RequestMethod.GET,value = "/health-check")
     public ResponseEntity<String> checkServerState() {
