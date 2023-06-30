f990ed95dc475d0f6cf01c5ebd23dd185648ccc7
ziogenorwekh <ziogenorwekh@gmail.com>
2023-06-30 18:57:54 +0900

    modified

--- src/main/java/com/example/projectboard/security/SecurityConfig.java
+++ src/main/java/com/example/projectboard/security/SecurityConfig.java
@@ -18,6 +18,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.channel.ChannelProcessingFilter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -54,7 +55,7 @@ public class SecurityConfig {
         http.authorizeHttpRequests().requestMatchers(HttpMethod.PUT).hasAnyRole("USER", "ADMIN");
         http.authorizeHttpRequests().requestMatchers(HttpMethod.DELETE).hasAnyRole("USER", "ADMIN");
 
-        http.addFilterBefore(new CorsFilter(), UsernamePasswordAuthenticationFilter.class)
+        http.addFilterBefore(new CorsFilter(), ChannelProcessingFilter.class)
                 .addFilter(new CustomizedAuthenticationFilter(authenticationManager(authenticationConfiguration()), jwts))
                 .addFilterBefore(new CustomizedAuthorizationFilter(authenticationManager(authenticationConfiguration()), jwts),
                         UsernamePasswordAuthenticationFilter.class);
